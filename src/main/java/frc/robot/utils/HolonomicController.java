fe244ad8c5160bf9b6a73490e6e52fa2ffa6933c
PGgit08 <peter.mty8@gmail.com>
2025-01-08 22:26:30 -0500

    spotless

--- src/main/java/frc/robot/utils/HolonomicController.java
+++ src/main/java/frc/robot/utils/HolonomicController.java
@@ -3,9 +3,7 @@ package frc.robot.utils;
 import edu.wpi.first.math.controller.PIDController;
 
 public class HolonomicController {
-    private final PIDController _xController = new PIDController(0, 0, 0);
-    private final PIDController _yController = new PIDController(0, 0, 0);
-    private final PIDController _headingController = new PIDController(0, 0, 0);
-
-    
+  private final PIDController _xController = new PIDController(0, 0, 0);
+  private final PIDController _yController = new PIDController(0, 0, 0);
+  private final PIDController _headingController = new PIDController(0, 0, 0);
 }
