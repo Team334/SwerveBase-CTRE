2ef54fb0c00f85c33d8f1ba8b56a8861b8ec4f17
PGgit08 <peter.mty8@gmail.com>
2025-09-25 21:32:33 -0400

    mostly added drive-to part of holonomic controller back in

--- src/main/java/frc/robot/utils/HolonomicController.java
+++ src/main/java/frc/robot/utils/HolonomicController.java
@@ -1,12 +1,27 @@
 package frc.robot.utils;
 
 import dev.doglog.DogLog;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 
 public class HolonomicController {
-  // used to follow the path
+  private final ProfiledPIDController _translationProfile =
+      new ProfiledPIDController(0, 0, 0, new Constraints(0, 0));
+  private final ProfiledPIDController _headingProfile =
+      new ProfiledPIDController(0, 0, 0, new Constraints(Math.PI, Math.PI * 2));
+
+  private Vector<N2> _translationDirection = VecBuilder.fill(0, 0);
+
+  private Pose2d _startPose = Pose2d.kZero;
+  private double _goalHeading = 0;
+
   private final PIDController _xController = new PIDController(0, 0, 0);
   private final PIDController _yController = new PIDController(0, 0, 0);
 
@@ -14,16 +29,86 @@ public class HolonomicController {
 
   public HolonomicController() {
     _headingController.enableContinuousInput(-Math.PI, Math.PI);
+    _headingProfile.enableContinuousInput(-Math.PI, Math.PI);
+  }
+
+  // TODO: add is finished condition
+
+  /** Resets the PID controllers. */
+  public void reset() {
+    _xController.reset();
+    _yController.reset();
+
+    _headingController.reset();
   }
 
   /**
-   * Modifies some base chassis speeds the drive is currently traveling at to bring the drive closer
-   * to a desired pose.
+   * Resets the motion profiles and the PID controllers.
    *
-   * @param baseSpeeds The field-relative speed the drive is already traveling at.
+   * @param currentPose The current pose.
+   * @param goalPose The goal pose.
+   * @param currentSpeeds The current field-relative speeds of the chassis.
+   */
+  public void reset(Pose2d currentPose, Pose2d goalPose, ChassisSpeeds currentSpeeds) {
+    _translationDirection =
+        VecBuilder.fill(goalPose.getX() - currentPose.getX(), goalPose.getY() - currentPose.getY());
+
+    _translationProfile.reset(
+        0,
+        _translationDirection.dot(
+                VecBuilder.fill(currentSpeeds.vxMetersPerSecond, currentSpeeds.vyMetersPerSecond))
+            / _translationDirection.norm());
+    _headingProfile.reset(
+        currentPose.getRotation().getRadians(), currentSpeeds.omegaRadiansPerSecond);
+
+    reset();
+
+    _startPose = currentPose;
+    _goalHeading = goalPose.getRotation().getRadians();
+  }
+
+  /**
+   * Samples the motions profiles at the next timestep, finding chassis speeds based on the profiles
+   * and PID correction.
+   *
+   * @param currentPose The current pose.
+   * @return Field-relative speeds for the chassis.
+   */
+  public ChassisSpeeds calculate(Pose2d currentPose) {
+    _translationProfile.calculate(
+        0,
+        _translationDirection.norm()); // measurement doesn't matter, handled by xy PID controllers
+    _headingProfile.calculate(
+        currentPose.getRotation().getRadians(), _goalHeading); // TODO: why does measurement matter?
+
+    Vector<N2> setpointPosition =
+        _translationDirection.unit().times(_translationProfile.getSetpoint().position);
+    Vector<N2> setpointVelocity =
+        _translationDirection.unit().times(_translationProfile.getSetpoint().velocity);
+
+    Pose2d setpointPose =
+        new Pose2d(
+            _startPose.getX() + setpointPosition.get(0),
+            _startPose.getY() + setpointPosition.get(1),
+            new Rotation2d(_headingProfile.getSetpoint().position));
+
+    return calculate(
+        new ChassisSpeeds(
+            setpointVelocity.get(0),
+            setpointVelocity.get(1),
+            _headingProfile.getSetpoint().velocity),
+        setpointPose,
+        currentPose);
+  }
+
+  /**
+   * Modifies some base field-relative chassis speeds the chassis is currently traveling at to bring
+   * it closer to the desired pose.
+   *
+   * @param baseSpeeds The field-relative speed the chassis is already traveling at.
    * @param desiredPose The desired pose.
-   * @param currentPose The current pose of the drive.
-   * @return New modified speeds.
+   * @param currentPose The current pose.
+   * @return New modified field-relative speeds.
    */
   public ChassisSpeeds calculate(ChassisSpeeds baseSpeeds, Pose2d desiredPose, Pose2d currentPose) {
     DogLog.log("Auto/Controller Desired Pose", desiredPose);
